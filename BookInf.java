ff5a83f4708f59d6493f728abeb03e1367c91b7f
Myxazaebyxa <97283442+Myxazaebyxa@users.noreply.github.com>
2026-08-18 04:23:15 +0300

    Delete BookInf.java

--- BookInf.java
+++
@@ -1,9 +0,0 @@
-public class BookInf {
-    public static void main (String[] args){
-        Book book = new Book("Стивен Кинг",2014,815,"11.22.63");
-        System.out.println(book.isBig());
-        System.out.println(book.estimatePrice());
-        System.out.println(book.matches("Кинг"));
-        System.out.println(book.matches("22"));
-    }
-}
